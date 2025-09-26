69c47eb3c533b82bfa6b24d617c2a9613d37b5a0
Satya <deploywithsatya@gmail.com>
2025-09-26 15:37:48 -0400

    basic cal

--- Day_1_cal.sh
+++ Day_1_cal.sh
@@ -3,7 +3,7 @@
 x=10
 y=20
 
-echo "Print the sum of both: $x+$y"
-echo "Print the obb of both:$x-$y"
-echo "Print of both multi : $x*$y"
-echo "Print of the devide : $x/$y"
+echo "Print the sum of both: $((x+y))"
+echo "Print the obb of both:$((x-y))"
+echo "Print of both multi : $((x*y))"
+echo "Print of the devide : $((x/y))"
