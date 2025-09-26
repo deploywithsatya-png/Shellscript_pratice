d4d084b3759a6b840845c476291fbbe9cd2f712b
Satya <deploywithsatya@gmail.com>
2025-09-26 15:24:02 -0400

    just var declartion by name

---
+++ Day_1_var.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+Person1=satya
+Person2=sai
+
+echo "$Person1: Hello $Person2 , How are you doing today"
+echo "$Person2: Hi $Person1, I'm doing good , Thank you, How are you"
+echo "$Person1: How is you work going on $Person2"
+echo "$Person2: Just going on man $Person1"
+echo "$Person1: Alright man, Take it easy $Person2"
+echo "$Person2: Okay Man see you $Person1"
